82db2a08e6c7ba46b59bb4fcd1340d8ec72fed3c
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2024-01-10 14:43:17 -0600

    first commit

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <button @click="reset">Reset</button>
+</template>
+
+<script setup>
+
+import { ref } from "vue";
+
+const count = ref(0); //propiedad que hago reactiva
+
+const increment = () => {
+  return count.value++;
+};
+
+const decrement = () => {
+  return (count.value = count.value - 1);
+};
+
+const reset = () => {
+  return (count.value = 0);
+};
+
+
+</script>
